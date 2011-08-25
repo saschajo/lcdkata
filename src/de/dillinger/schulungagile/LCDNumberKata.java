f7cf91fd7c002a8de6bc34c946d32b25a94dbd54
saschajo <saschajo@saschas-lappi>
2011-08-25 16:28:11 +0200

    Test

---
+++ src/de/dillinger/schulungagile/LCDNumberKata.java
@@ -0,0 +1,13 @@
+package de.dillinger.schulungagile;
+
+public class LCDNumberKata {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/de/dillinger/schulungagile/test/LCDNumberKataTestCase.java
@@ -0,0 +1,14 @@
+package de.dillinger.schulungagile.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LCDNumberKataTestCase {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
